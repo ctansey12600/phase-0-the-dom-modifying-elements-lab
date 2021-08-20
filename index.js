e720b7eb4c97fe2b941814bfc91e011394539154
caseytansey <ctansey12600@outlook.com>
2021-08-20 14:41:32 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ main.remove()
 
 const newHeader = document.createElement("h1")
 const body = document.querySelector("body")
-body.append(newHeader)
+
 newHeader.id = "victory"
 newHeader.textContent = "Casey is the champion"
 
+body.append(newHeader)

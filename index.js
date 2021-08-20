47f20a3ff675c50ea1dacdcad667fba67bbe34d0
caseytansey <ctansey12600@outlook.com>
2021-08-20 14:40:40 -0500

    commit tree 39ad13a2ff80e50520dd3b66fb9f422a10040c8b

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ main.remove()
 
 const newHeader = document.createElement("h1")
 const body = document.querySelector("body")
-body.append(newHeader)
+
+console.log(newHeader)
 newHeader.id = "victory"
 newHeader.textContent = "Casey is the champion"
 
+body.append(newHeader)
